53eb0a821a3d141358a960603504a2364e5ba1d8
Misha Panyushkin <misha.panyushkin@gmail.com>
2013-07-15 19:59:41 +0400

    adding demo page

---
+++ open_lesson_demos/js/stretch_demo.simple.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+    $.backstretch([
+        "http://ia.media-imdb.com/images/M/MV5BMjEyMjgzNjIxOV5BMl5BanBnXkFtZTcwOTg3OTgwMw@@._V1._SX640_SY572_.jpg"
+        , "http://ia.media-imdb.com/images/M/MV5BMzQ3MTQzMjU5MV5BMl5BanBnXkFtZTcwMjg3OTgwMw@@._V1._SX640_SY460_.jpg"
+        , "http://ia.media-imdb.com/images/M/MV5BMjA0MzU5NjkyN15BMl5BanBnXkFtZTcwODU3OTgwMw@@._V1._SX640_SY399_.jpg"
+        , "http://ia.media-imdb.com/images/M/MV5BMjE5MDk3MDQ0NV5BMl5BanBnXkFtZTcwMzc3OTgwMw@@._V1._SX640_SY425_.jpg"
+    ], {duration: 3000, fade: 750});
+});
