c2ea89b524405f02eddc9a9b7b0365d40dba312d
Misha Panyushkin <misha.panyushkin@gmail.com>
2013-10-01 12:48:26 +0400

    Adding essential files: - three lesson html markups; - one open lesson html markup; - static pictures folder; - shower framework folder; - practice examples folder.

---
+++ js_for_practice/ArraysToJSON.js
@@ -0,0 +1,22 @@
+// Имена.
+var Jean = ["Jean","https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQO9OLSOvOsGiGcbK6ZYD6m07BVYNDbDoNbOF5XbQglspDpCFKU0Q", "L?on"];
+var Natali = ["Natali", "http://www.empireonline.com/images/features/100greatestcharacters/photos/62.jpg", "Mathilda"];
+var Gary = ["Gary","http://images2.wikia.nocookie.net/__cb20110523165431/villains/images/7/7f/Stansfield.jpg", "Stansfield"];
+
+var actors = {
+    "Jean" : {
+        "name"  : "Jean",
+        "ava"   : "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQO9OLSOvOsGiGcbK6ZYD6m07BVYNDbDoNbOF5XbQglspDpCFKU0Q",
+        "character" : "L?on"
+    },
+    "Natali" : {
+        "name"  : "Natali",
+        "ava"   : "http://www.empireonline.com/images/features/100greatestcharacters/photos/62.jpg",
+        "character" : "Mathilda"
+    },
+    "Gary" : {
+        "name"  : "Gary",
+        "ava"   : "http://images2.wikia.nocookie.net/__cb20110523165431/villains/images/7/7f/Stansfield.jpg",
+        "character" : "Stansfield"
+    }
+};
